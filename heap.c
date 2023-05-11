069acf962a67600f4df977f0dc63b054671e360f
Replit user <>
2023-05-11 17:11:16 +0000

    new try

--- heap.c
+++ heap.c
@@ -17,9 +17,12 @@ typedef struct Heap{
 } Heap;
 
 
-void* heap_top(Heap* pq){
-    return NULL;
-}
+void* heap_top(Heap* pq)
+{
+  if(pq->size == 0) return NULL;
+
+  return pq->heapArray[0]->data;
+  }
 
 
 
@@ -30,9 +33,7 @@ void heap_push(Heap* pq, void* data, int priority){
 
 void heap_pop(Heap* pq)
 {
-  if(pq->size == 0) return NULL;
-
-  return pq->heapArray[0].data;
+  
 }
 
 Heap* createHeap(){
